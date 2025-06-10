f38fe394d5a4bd36aab871ea1d4bfd0d46723c62
Doan Hien <doanhien3303@gmail.com>
2025-06-10 09:46:51 +0700

    Remove binary file

--- EventTrackingVer2/Components/Pages/Events/EventDetail.razor.cs
+++ EventTrackingVer2/Components/Pages/Events/EventDetail.razor.cs
@@ -12,21 +12,15 @@ namespace EventTrackingVer2.Components.Pages.Events
         [Parameter] public int EventId { get; set; }
 
         private Event eventDetail = new();
-        private List<Guest> guestList = new();
-        private RadzenDataGrid<Guest> guestGrid;
+        private List<GuestList> guestList = new();
 
         private IRadzenFormComponent editor;
         private bool editorFocused;
 
-        private List<Guest> guestsToUpdate = new();
-        private List<KeyValuePair<int, string>> editedFields = new();
-
-        private string columnEditing;
+      
         private string? message;
 
-        private List<string> statusOptions = new() { "Unconfirm", "Confirmed" };
-        private List<string> resultOptions = new() { "Absent", "Present" };
-
+       
         protected override async Task OnInitializedAsync()
         {
             eventDetail = await DbContext.Events
@@ -56,62 +50,7 @@ namespace EventTrackingVer2.Components.Pages.Events
             }
         }
 
-        private async Task OnCellClick(DataGridCellMouseEventArgs<Guest> args)
-        {
-            if (!guestGrid.IsValid ||
-                guestsToUpdate.Contains(args.Data) && columnEditing == args.Column.Property) return;
-
-            if (guestsToUpdate.Any())
-            {
-                editedFields.Add(new(guestsToUpdate.First().Id, columnEditing));
-                await Update();
-            }
-
-            columnEditing = args.Column.Property;
-            await EditRow(args.Data);
-        }
-
-        private async Task EditRow(Guest guest)
-        {
-            Reset();
-            if (!guestsToUpdate.Contains(guest))
-                guestsToUpdate.Add(guest);
-
-            await guestGrid.EditRow(guest);
-        }
-
-        private async Task Update()
-        {
-            editorFocused = false;
-
-            if (guestsToUpdate.Any())
-            {
-                await guestGrid.UpdateRow(guestsToUpdate.First());
-            }
-        }
-
-        private void Reset(Guest guest = null)
-        {
-            editorFocused = false;
-
-            if (guest != null)
-                guestsToUpdate.Remove(guest);
-            else
-                guestsToUpdate.Clear();
-        }
-
-        private bool IsEditing(string columnName, Guest guest)
-        {
-            return columnEditing == columnName && guestsToUpdate.Contains(guest);
-        }
-
-        private async Task OnUpdateRow(Guest guest)
-        {
-            Reset(guest);
-            DbContext.Update(guest);
-            await DbContext.SaveChangesAsync();
-        }
-
+       
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (!editorFocused && editor != null)
---
+++ EventTrackingVer2/Components/Pages/Guest/GuestList.razor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Radzen;
+using Radzen.Blazor;
+using EventTrackingVer2.Models;
+
+namespace EventTrackingVer2.Components.Pages.Guest
+{
+    public partial class GuestList
+    {
+        [Parameter] public List<GuestList>? guestLists { get; set; }
+        [Parameter] public EventCallback<GuestList> OnUpdateGuest { get; set; }
+        private List<GuestList> guestsToUpdate = new();
+        private RadzenDataGrid<GuestList>? guestGrid;
+        private List<string> statusOptions = new() { "Unconfirm", "Confirmed" };
+        private List<string> resultOptions = new() { "Absent", "Present" };
+        private string columnEditing;
+        private bool editorFocused;
+        private List<KeyValuePair<int, string>> editedFields = new();
+
+        private async Task OnCellClick(DataGridCellMouseEventArgs<GuestList> args)
+        {
+            if (!guestGrid.IsValid ||
+                guestsToUpdate.Contains(args.Data) && columnEditing == args.Column.Property) return;
+
+            if (guestsToUpdate.Any())
+            {
+                editedFields.Add(new(guestsToUpdate.First().Id, columnEditing));
+                await Update();
+            }
+
+            columnEditing = args.Column.Property;
+            await EditRow(args.Data);
+        }
+
+        private async Task EditRow(GuestList guest)
+        {
+            Reset();
+            if (!guestsToUpdate.Contains(guest))
+                guestsToUpdate.Add(guest);
+
+            await guestGrid.EditRow(guest);
+        }
+
+        private async Task Update()
+        {
+            editorFocused = false;
+
+            if (guestsToUpdate.Any())
+            {
+                await guestGrid.UpdateRow(guestsToUpdate.First());
+            }
+        }
+
+        private void Reset(GuestList guest = null)
+        {
+            editorFocused = false;
+
+            if (guest != null)
+                guestsToUpdate.Remove(guest);
+            else
+                guestsToUpdate.Clear();
+        }
+
+        private bool IsEditing(string columnName, GuestList guest)
+        {
+            return columnEditing == columnName && guestsToUpdate.Contains(guest);
+        }
+
+        private async Task OnUpdateRow(GuestList guest)
+        {
+            Reset(guest);
+            DbContext.Update(guest);
+            await DbContext.SaveChangesAsync();
+        }
+
+    }
+}
